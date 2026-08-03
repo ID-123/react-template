be28f900e102add9bac099ed543245891a71387f
ID-123 <unityplayer12@gmail.com>
2026-08-03 05:20:59 -0500

    navigation created

---
+++ src/config/navigation/guestLinks.ts
@@ -0,0 +1,19 @@
+// Navigation links for guest users (not logged in)
+export const guestLinks = [
+    {
+        label: "Home",
+        to: "/"
+    },
+    {
+        label: "Movies",
+        to: "/movies"
+    },
+    {
+        label: "Login",
+        to: "/login"
+    },
+    {
+        label: "Register",
+        to: "/register"
+    }
+];
